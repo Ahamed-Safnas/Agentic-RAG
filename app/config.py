c4951f3cbb35642ea55d9aa67abc4eb766e51996
safnasNED25 <safnas2042ahamed@gmail.com>
2026-09-19 23:09:51 +0530

    config updated

--- app/config.py
+++ app/config.py
@@ -15,7 +15,7 @@ class Settings:
 
     # --- REASONING ENGINE (GROQ) ---
     GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-    GROQ_MODEL = "llama-3.3-70b-versatile"
+    GROQ_MODEL = "openai/gpt-oss-20b"
     GROQ_FALLBACK_API_KEY = os.getenv("GROQ_FALLBACK_API_KEY")
 
 settings = Settings()
